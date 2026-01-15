dd1c374048e067610f1d4493e9212c174ebda226
alpha-lantern <alvinjoelts@gmail.com>
2026-01-15 14:04:01 -0500

    header corrections for the assignment

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,9 +1,7 @@
 const menu = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
-const header = document.querySelector('header');
 
 menu.addEventListener('click', () => {
     navigation.classList.toggle('open');
     menu.classList.toggle('open');
-    header.classList.toggle('open');
 });
