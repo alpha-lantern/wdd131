52cf3304d8b0d4c849a506b0250c0133f6201fbb
alpha-lantern <alvinjoelts@gmail.com>
2026-01-19 16:43:20 -0500

    week03 Learning Activities

---
+++ week03/scripts/functions.js
@@ -0,0 +1,18 @@
+let firstName = 'Antonia';
+let lastName = 'Francesca';
+
+// function fullName (first, last) {
+//     return `${first} ${last}`;
+//     // return first + ' ' + last; (not preferred)
+// };
+
+// const fullName = function (first, last) {
+//     return `${first} ${last}`;
+// };
+
+const fullName = ((firstName, lastName) => `${firstName} ${lastName}`);
+
+document.querySelector('#fullName').innerHTML = fullName(firstName, lastName);
+
+
+
